4f26c107c8f54f07ca377a342b31a3084d7a85d2
jkamata <haikarasha@gmail.com>
2017-10-24 00:41:57 +0900

    add lua.

--- prog1.cc
+++ prog1.cc
@@ -2,11 +2,23 @@
 // C++ Program 1
 
 #include <iostream>
+#include <lua.hpp>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-  cout << "This is prog1.\n";
+  /* Luaのステートオブジェクトの作成 */
+  lua_State *lua = luaL_newstate();
+
+  /* Luaのライブラリの読込み */
+  luaL_openlibs(lua);
+
+  /* Luaのスクリプトの実行 */
+  luaL_dofile(lua, "hello.lua");
+
+  /* Luaのステートオブジェクトの開放 */
+  lua_close(lua);
+
   return 0;
 }
